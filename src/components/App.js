ca42815e71219926ec6bc5a2bee2db12c2134239
Gaitano <gaitano.george@student.moringaschool.com>
2023-08-01 13:12:39 +0300

    Added footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+// import './App.css';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
      <h1>HomePage</h1>
+     <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <div>
+            <div>
+                <div>
+                    <h3>Newletter</h3>
+                    <p>Enter your e-mail address,and we promise not to send spam mails.</p>
+                </div>
+                <div>
+                    <h3>Contact Us</h3>
+                    <p><i class="fa-regular fa-location-dot"></i>Ngong Road</p>
+                    <p><i class="fa-solid fa-phone"></i> +254 134 765 890/ +254 784 000 555</p>
+                    <p><i class="fa-regular fa-envelope"></i> patanafuu@gmail.com</p>
+                </div>
+                <div>
+                    <i class="fa-brands fa-instagram"></i>
+                    <i class="fa-brands fa-facebook"></i>
+                    <i class="fa-brands fa-linkedin"></i>
+                </div>
+            </div>
+            <div>
+                <p>Pata-Nafuu Copyright © 2022 All Rights Reserved</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
