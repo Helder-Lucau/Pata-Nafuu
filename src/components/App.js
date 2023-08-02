37cb0abd09b8c6c66acf8a7c2644de08a6eeaf73
Gaitano <gaitano.george@student.moringaschool.com>
2023-08-03 00:52:19 +0300

    removed changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import AboutUs from '../AboutUs';
+import "../App.css";
 
 function App() {
   return (
     <div className="App">
-     <AboutUs />
+
     </div>
   );
 }
