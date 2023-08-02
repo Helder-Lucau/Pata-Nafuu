1e678fee3a614aa065d6e32616fa0df3fceb8bce
Helder <helderlucau@gmail.com>
2023-08-03 01:03:47 +0300

    merged files

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 import '../App.css';
 import BookAppointment from './BookAppointment';
 import Footer from './Footer';
-function App() {
-  return (
-    <div className="App">
+
 
 function App() {
   return (
     <div className="App">
      <BookAppointment />
+     <Footer />
     </div>
   );
 }
