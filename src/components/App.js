b0af580213bc1562718e93ce20d36306e9af4c5c
Helder <helderlucau@gmail.com>
2023-08-03 13:54:50 +0300

    editing files

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../style/App.css";
-import Footer from "./Footer";
 import Home from "./Home";
 import CardInfo from "./Teamsection";
 import Navbar from "./Navbar";
@@ -15,8 +14,7 @@ function App() {
           <h1> MEET OUR TEAM</h1>
           <CardInfo />
         </div>
-    <Footer />
-      </div>
+        </div>
     </div>
   );
 }
