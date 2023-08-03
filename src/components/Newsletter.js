b516d0048898d02bad346f26e784fb94ef897f8d
BRIAN-M@RTIN <+>
2023-08-03 16:44:52 +0300

    added Api

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000; // You can change this to your desired port number
 
-app.use(express.json());
+//app.use(express.json());
 
 // Mock database to store subscribers (You can use a real database in a production environment)
-const subscribers = [];
+//const subscribers = [];
 
 // API endpoint to handle newsletter subscriptions
 app.get('/subscribe', (req, res) => {
